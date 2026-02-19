bec519079235c6e16f6c5c9f22dc65fd414882f7
Autra <primechris72@gmail.com>
2026-02-19 11:24:19 +0200

    uploaded mp3s

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const open = document.getElementById('cont');
-const pop = document.getElementById('modal');
-
-open.addEventListener('click', ()=>{
-    pop.classList.add('show');
-}) 
+function downloadSong() {
+            var downloadUrl = 'https://github.com/AUTRACHRIS-TAO/AutraBeats/raw/refs/heads/main/Kanjedza(Intro).mp3';
+            
+            var link = document.createElement('a');
+            link.href = downloadUrl;
+            link.download = ''; 
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
